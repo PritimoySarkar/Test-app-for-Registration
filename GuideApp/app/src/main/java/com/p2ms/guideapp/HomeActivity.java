bc15a40b730e56fbae7fb55e10db950f478a102f
Pritimoy Sarkar <pritimoysarkar@gmail.com>
2020-08-27 10:24:34 +0530

    gitignore fixed

--- GuideApp/app/src/main/java/com/p2ms/guideapp/HomeActivity.java
+++ GuideApp/app/src/main/java/com/p2ms/guideapp/HomeActivity.java
@@ -3,12 +3,17 @@ package com.p2ms.guideapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
-
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        btnLogin = findViewById(R.id.logOutBtn);
+
+        //btnLogin.setOnClickListener();
     }
 }
